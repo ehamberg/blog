3c70ffc7c8ab2255ec1f5d16cb6132e170d023a6
Erlend Hamberg <ehamberg@gmail.com>
2012-05-27 19:51:25 +0200

    fix 404 page

--- blog.hs
+++ blog.hs
@@ -86,18 +86,12 @@ main = hakyllWith config $ do
     match "templates/*" $ compile templateCompiler
 
     -- Render some static pages
-    forM_ ["about.markdown", "resume.markdown", "projects.markdown"] $ \p ->
+    forM_ ["404.markdown", "about.markdown", "resume.markdown", "projects.markdown"] $ \p ->
         match p $ do
             route   $ setExtension ".html"
             compile $ pageCompiler
                 >>> applyTemplateCompiler "templates/default.html"
                 >>> relativizeUrlsCompiler
-
-    -- Render the 404 page, we don't relativize URL's here.
-    match "404.html" $ do
-        route idRoute
-        compile $ pageCompiler
-            >>> applyTemplateCompiler "templates/default.html"
 
     -- Render RSS feed
     match "rss.xml" $ route idRoute
